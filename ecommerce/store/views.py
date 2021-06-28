b7a1743b9681985c6e2498ba51a40d8e7de9c2cd
khongchai <world1955@hotmail.com>
2021-06-28 21:48:36 +0700

    checkout is done is backend

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
-
+import datetime
 
 def store(request):
     products = Product.objects.all()
@@ -44,3 +44,32 @@ def update_item(request):
     #from docs, :param safe: Controls if only ``dict`` objects may be serialized. Defaults
     #to ``True``.
     return JsonResponse("Item was added", safe=False)
+
+def process_order(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        cart, _ = Cart.objects.get_or_create(customer=customer, complete="False")
+        print(data["userFormData"])
+        total = float(data["userFormData"]["total"])
+        cart.transaction_id = transaction_id
+
+        #check if total sent is the same as the cart total
+        if total == cart.get_cart_total:
+            cart.complete = True
+        cart.save()
+
+        if cart.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                cart=cart,
+                address=data["shippingInfo"]["address"],
+                city=data["shippingInfo"]["city"],
+                state=data["shippingInfo"]["state"],
+                zipcode=data["shippingInfo"]["zipcode"]
+            )
+
+    return JsonResponse("Payment Complete", safe=False)
+
